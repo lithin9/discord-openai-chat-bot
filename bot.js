4dc95d73fe2b1650ade106e9d8655a8465e16606
Robert Van Loon <robert.vanloon4@gmail.com>
2025-01-12 16:47:00 -0600

    added process exit logging to know how the program exited

--- bot.js
+++ bot.js
@@ -4,4 +4,16 @@ const { SapphireClient } = require('@sapphire/framework');
 const token = process.env.DISCORD_BOT_TOKEN;
 const client = new SapphireClient({ intents: ['GUILDS', 'GUILD_MESSAGES'] });
 
-client.login(token);
+client.login(token)
+
+process.on('uncaughtException', (err) => {
+	console.error('Uncaught Exception:', err);
+});
+process.on('SIGTERM', () => {
+	console.log('Process terminating...');
+	process.exit(0);
+});
+process.on('SIGINT', () => {
+	console.log('Process interrupted...');
+	process.exit(0);
+});
